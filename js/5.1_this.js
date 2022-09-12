1acfdef765d36fd65d98c42ad4189b043b9c7bce
NickNedilko <overlord311089@gmail.com>
2022-09-12 17:15:11 +0300

    Update 5.1_this.js

--- js/5.1_this.js
+++ js/5.1_this.js
@@ -14,38 +14,136 @@
 
 // =============================================================================================
 
-class User {
-  // Необязательное объявление публичных свойств
-  name;
-  // Обязательное объявление приватных свойств
-  #email;
+// class User {
+//   // Необязательное объявление публичных свойств
+//   name;
+//   // Обязательное объявление приватных свойств
+//   #email;
 
-  constructor({ name, email }) {
-    this.name = name;
-    this.#email = email;
-  }
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
 
-  getEmail() {
-    return this.#email;
-  }
+//   getEmail() {
+//     return this.#email;
+//   }
 
-  changeEmail(newEmail) {
-    this.#email = newEmail;
-  }
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+
+// const kolyan = new User({
+//   name: "Kolya",
+//   email: "kolya@email.ru",
+// });
+// kolyan.changeEmail("kolyan@supermail.ru");
+// console.log(kolyan.getEmail());
+
+// mango.changeEmail("mango@supermail.com");
+// console.log(mango.getEmail()); // mango@supermail.com
+// // console.log(mango.#email); // Будет ошибка, это приватное свойство
+
+// ===================================================================================================
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   // Метод getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Метод changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+//   addNewPar(newName) {
+//     this.name = newName;
+//   }
+// }
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// console.log(mango); // { name: "Манго", email: "mango@mail.com" }
+
+// const poly = new User({
+//   name: "Поли",
+//   email: "poly@mail.com",
+// });
+// console.log(poly); // { name: "Поли", email: "poly@mail.com" }
+
+// poly.changeEmail('red@gmail.com')
+// console.log(poly); // { name: "Поли", email: "poly@mail.com" }
+// poly.addNewPar('red')
+// console.log(poly)
+
+// ==================================================================================
+// class User {
+//   // Необязательное объявление публичных свойств
+//   name;
+//   // Обязательное объявление приватных свойств
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// mango.changeEmail("mango@supermail.com");
+// console.log(mango.getEmail()); // mango@supermail.com
+// console.log(mango.email); // Будет ошибка, это приватное свойство
+
+// ====================================Static method========================
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: "mango@mail.com" });
+
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+
+// ===================================================================
+function multiply(firstNumber, secondNumber, ...otherArgs) {
+  console.log(firstNumber); // Значение первого аргумента
+  console.log(secondNumber); // Значение второго аргумента
+  console.log(otherArgs); // Массив остальных аргументов
 }
 
-const mango = new User({
-  name: "Манго",
-  email: "mango@mail.com",
-});
-
-const kolyan = new User({
-  name: "Kolya",
-  email: "kolya@email.ru",
-});
-kolyan.changeEmail("kolyan@supermail.ru");
-console.log(kolyan.getEmail());
-
-mango.changeEmail("mango@supermail.com");
-console.log(mango.getEmail()); // mango@supermail.com
-// console.log(mango.#email); // Будет ошибка, это приватное свойство
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4,5,7,7,7);
